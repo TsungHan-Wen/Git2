fd48f67a47ed088596fe6f08ea970dc8c900d525
Wen <trytrycan13@gmail.com>
2021-03-05 01:55:59 +0800

    version1.3-2，本地分支，修改第10行程式碼

--- src/model/GitTest.java
+++ src/model/GitTest.java
@@ -7,7 +7,7 @@ public class GitTest {
 		System.out.println(7);  // version1.1-1，遠端master，修改第7行程式  --> 解決衝突後		
 		System.out.println(8);  // version1.1-2，本地master，修改第8行程式  --> 解決衝突後
 		
-		
+		System.out.println(10); // version1.3-2，本地分支，修改第10行程式碼
 		
 		
 		
