357dc71b90dc475227bff2476c639429ae8a204b
TsungHan-Wen <77219705+TsungHan-Wen@users.noreply.github.com>
2021-03-05 01:54:21 +0800

    Update GitTest.java
    version1.3-1，遠端master，修改第9行程式碼

--- src/model/GitTest.java
+++ src/model/GitTest.java
@@ -6,7 +6,7 @@ public class GitTest {
 		System.out.println(6);
 		System.out.println(7);  // version1.1-1，遠端master，修改第7行程式  --> 解決衝突後		
 		System.out.println(8);  // version1.1-2，本地master，修改第8行程式  --> 解決衝突後
-		
+		System.out.println(9);  // version1.3-1，遠端master，修改第9行程式碼
 		
 		
 		
