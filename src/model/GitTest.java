fdf95c293f0d49dde3f4900b540b363a9a01b831
Wen <trytrycan13@gmail.com>
2021-03-05 09:22:12 +0800

    version1.5-1，本地分支，修改第11行程式

--- src/model/GitTest.java
+++ src/model/GitTest.java
@@ -8,7 +8,7 @@ public class GitTest {
 		System.out.println(8);  // version1.1-2，本地master，修改第8行程式  --> 解決衝突後
 		System.out.println(9);  // version1.3-1，遠端master，修改第9行程式 -->解決衝突後	
 		System.out.println(10); // version1.3-2，本地分支，修改第10行程式，再merge到本地master -->解決衝突後
-		
+		System.out.println(11); // version1.5-1，本地分支，修改第11行程式
 		
 		
 		
