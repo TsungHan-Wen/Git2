e0300f85ebeed558f70e5c9cd4c3a6d9b971ce3a
Wen <trytrycan13@gmail.com>
2021-03-05 10:13:01 +0800

    version1.7 本地分支新增兩個檔案 File1、File2，merge到本地master

---
+++ src/model/File1.java
@@ -0,0 +1,22 @@
+package model;
+
+// 本地分支，新增File1檔案，merge進本地master
+public class File1 {
+
+	public static void main(String[] args) {
+		System.out.println(6);  
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ src/model/File2.java
@@ -0,0 +1,22 @@
+package model;
+
+//本地分支，新增File2檔案，merge進本地master
+public class File2 {
+
+	public static void main(String[] args) {
+		System.out.println(6);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
