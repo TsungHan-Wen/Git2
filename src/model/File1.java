1d6a50b0ac3a7818476e56a500887dd72b2d6ecc
Wen <trytrycan13@gmail.com>
2021-03-05 10:23:54 +0800

    version1.8-1 本地分支，修改File1檔案，第7行程式，再merge到本地master 本地分支，修改File1檔案，第8行程式，再merge到本地master

--- src/model/File1.java
+++ src/model/File1.java
@@ -4,8 +4,8 @@ package model;
 public class File1 {
 
 	public static void main(String[] args) {
-		System.out.println(6);  
-		
+		System.out.println(7);  // 本地分支，修改File1檔案，第7行程式，再merge到本地master
+		System.out.println(8);  // 本地分支，修改File1檔案，第8行程式，再merge到本地master
 		
 		
 		
